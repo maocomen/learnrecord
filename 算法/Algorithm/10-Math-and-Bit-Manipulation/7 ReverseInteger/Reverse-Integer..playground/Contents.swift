28eb7deaf4673c10cb2338d586989ce0416aa5a4
马鑫 <13554778196@163.com>
2026-07-19 04:59:04 +0800

    重新调整代码

--- 算法/Algorithm/10-Math-and-Bit-Manipulation/7 ReverseInteger/Reverse-Integer..playground/Contents.swift
+++ 算法/Algorithm/10-Math-and-Bit-Manipulation/7 ReverseInteger/Reverse-Integer..playground/Contents.swift
@@ -1,20 +1,31 @@
-//: A UIKit based Playground for presenting user interface
-  
+// ============================================
+// 题目: [整数反转]
+// 链接: [https://leetcode.cn/problems/reverse-integer/]
+// 难度: [中等]
+// 分类: [数学与位运算]
+// 解法: [数学法]
+// ============================================
+
+//还有字符串反转法、数组/栈法，递归法，位运算法，64位作弊法
+
 import UIKit
 
-func reverse(_ x: Int) -> Int {
-    var temp = x
-    var rev = 0
-    while temp != 0 {
-        if rev < Int32.min / 10 || rev > Int32.max / 10 {
-            return 0
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        var temp = x
+        var rev = 0
+        while temp != 0 {
+            if rev < Int32.min / 10 || rev > Int32.max / 10 {
+                return 0
+            }
+            let digit = temp % 10
+            temp /= 10
+            rev = rev * 10 + digit
         }
-        let digit = temp % 10
-        temp /= 10
-        rev = rev * 10 + digit
+        return rev
     }
-    return rev
 }
 
-let result = reverse(2147483647)
-print(result)
+let sol = Solution()
+sol.reverse(2147483647)
+sol.reverse(123)
